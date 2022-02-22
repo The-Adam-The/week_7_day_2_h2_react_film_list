f7858442e98715bbbf11d2dc2e258b236ed6dc80
codeclan student <codeclanstudent@codeclans-mbp.home>
2022-02-22 15:37:02 +0000

    created the functions for 'FilmBox', 'FilmList' and 'Film'.

---
+++ src/components/Film.js
@@ -0,0 +1,8 @@
+const Film = () => {
+
+    return (
+        <h3>I'm a Film!</h3>
+    );
+};
+
+export default Film
---
+++ src/components/FilmList.js
@@ -0,0 +1,14 @@
+import Film from './Film'
+
+const FilmList = () => {
+
+
+    return (
+    <>
+    <h2>I'm a film list!</h2>
+    <Film/>
+    </>
+    
+)};
+
+export default FilmList;
